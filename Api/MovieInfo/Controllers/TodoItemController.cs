7294ce61525e12ecce318d4fcb06c80b9488a87a
Amirhossein Firouzkouhi <a.firouzkouhi@DEV-56>
2022-01-22 18:11:05 +0330

    Adding api.

---
+++ Api/MovieInfo/Controllers/TodoItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieInfo.Models;
+
+namespace MovieInfo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly ILogger<TodoItemController> _logger;
+
+        public TodoItemController(ILogger<TodoItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "TodoItem")]
+        public TodoItem Get()
+        {
+            return new TodoItem();
+        }
+    }
+}
